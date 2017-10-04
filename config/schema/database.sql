933bcc7e87da7433ed1258968650cb335ff3ad6f
Joe Trumbull <trumbull.j@gmail.com>
2017-10-04 04:09:25 -0400

    Schema

--- config/schema/database.sql
+++ config/schema/database.sql
@@ -1,12 +1,14 @@
 USE `funds`;
 
 DROP TABLE IF EXISTS `accounts`;
+DROP TABLE IF EXISTS `attachments`;
 DROP TABLE IF EXISTS `comments`;
 DROP TABLE IF EXISTS `funds`;
 DROP TABLE IF EXISTS `investments`;
 DROP TABLE IF EXISTS `investors`;
 DROP TABLE IF EXISTS `offerings`;
 DROP TABLE IF EXISTS `transactions`;
+DROP TABLE IF EXISTS `users`;
 
 CREATE TABLE `accounts` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -26,7 +28,23 @@ CREATE TABLE `accounts` (
   `modified_by_id` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=880 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `attachments` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `parent` varchar(250) DEFAULT NULL,
+  `parent_id` int(10) unsigned DEFAULT NULL,
+  `name` varchar(250) DEFAULT NULL,
+  `path` varchar(250) DEFAULT NULL,
+  `mime` varchar(250) DEFAULT NULL,
+  `size` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `created_by_id` int(10) unsigned DEFAULT NULL,
+  `modified_by_id` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `comments` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -39,7 +57,7 @@ CREATE TABLE `comments` (
   `modified_by_id` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `funds` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -57,26 +75,36 @@ CREATE TABLE `funds` (
   `modified_by_id` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `investments` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `offering_id` int(10) unsigned DEFAULT NULL,
   `account_id` int(10) unsigned DEFAULT NULL,
   `client_id` int(11) DEFAULT NULL,
+  `rollover` tinyint(1) unsigned DEFAULT '0',
   `date` date DEFAULT NULL,
   `amount` decimal(15,2) DEFAULT NULL,
   `term` int(5) DEFAULT '36',
+  `maturity` date DEFAULT NULL,
   `balance` decimal(15,2) DEFAULT NULL,
   `preferred_payment` decimal(15,2) DEFAULT NULL,
   `status` int(5) DEFAULT NULL,
+  `closed` date DEFAULT NULL,
+  `ach` tinyint(1) unsigned DEFAULT NULL,
+  `ach_bank` varchar(250) DEFAULT NULL,
+  `ach_routing` varchar(250) DEFAULT NULL,
+  `ach_account` varchar(250) DEFAULT NULL,
+  `ach_type` int(10) unsigned DEFAULT NULL,
+  `statements_start` date DEFAULT NULL,
+  `statements_end` date DEFAULT NULL,
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   `created_by_id` int(10) unsigned DEFAULT NULL,
   `modified_by_id` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=927 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `investors` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -95,11 +123,12 @@ CREATE TABLE `investors` (
   `modified_by_id` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=457 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `offerings` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `fund_id` int(10) unsigned DEFAULT NULL,
+  `date` date DEFAULT NULL,
   `class` varchar(10) DEFAULT NULL,
   `rate` decimal(10,5) DEFAULT NULL,
   `capacity` decimal(15,2) DEFAULT NULL,
@@ -112,7 +141,22 @@ CREATE TABLE `offerings` (
   `modified_by_id` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `statements` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `investment_id` int(10) unsigned DEFAULT NULL,
+  `correct` tinyint(1) DEFAULT '1',
+  `date` date DEFAULT NULL,
+  `name` varchar(250) DEFAULT NULL,
+  `size` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `created_by_id` int(10) unsigned DEFAULT NULL,
+  `modified_by_id` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=22912 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `transactions` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
@@ -126,4 +170,19 @@ CREATE TABLE `transactions` (
   `modified_by_id` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=14589 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `active` tinyint(1) DEFAULT '0',
+  `locked` tinyint(1) DEFAULT '0',
+  `token` char(36) DEFAULT NULL,
+  `token_type` varchar(50) DEFAULT NULL,
+  `username` varchar(250) DEFAULT NULL,
+  `password` varchar(250) DEFAULT NULL,
+  `role` int(5) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8;
